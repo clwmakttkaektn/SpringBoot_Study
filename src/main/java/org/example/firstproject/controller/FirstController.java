e895ef9bcc86026e16e26e451b7e2196fb5771c0
taekyu kim <kimtaekyu1204@naver.com>
2024-07-18 18:12:04 +0900

    0718_first

--- src/main/java/org/example/firstproject/controller/FirstController.java
+++ src/main/java/org/example/firstproject/controller/FirstController.java
@@ -10,9 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class FirstController {
 
-    @GetMapping("/hi")
+    @GetMapping("/hello")
     public String niceToMeetYou(Model model){
         model.addAttribute("username","홍팍");
         return "greetings";
     }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname","홍길동");
+        return "goodbye";
+    }
+
+
 }
